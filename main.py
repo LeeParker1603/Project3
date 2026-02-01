2d010c056d39c67bfb03bf53481ae6333dc97ca3
Евгения Горобец <leeparker1603@gmail.com>
2026-02-01 14:43:18 +0700

    latest red. 01/02

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-from src.widget import mask_account_card, get_date
+from src.widget import get_date, mask_account_card
 
 if __name__ == "__main__":
     input_info = "Visa Classic 6831982476737658"
-    #input_info = "счет 56456831982476737658"
+    # input_info = "счет 56456831982476737658"
     input_date = "2024-03-11T02:26:18.671407"
 
     print(mask_account_card(input_info))
