c92e6012c48ff9529fac3ff9960e341b9d739948
Евгения Горобец <leeparker1603@gmail.com>
2026-02-04 22:17:18 +0700

    correct homework_10_1 - flake8 max-line-length = 79

--- src/processing.py
+++ src/processing.py
@@ -1,11 +1,14 @@
 from typing import Dict, List
 
 
-def filter_by_state(user_data_list: List[Dict], state: str = "EXECUTED") -> List[Dict]:
+def filter_by_state(
+    user_data_list: List[Dict], state: str = "EXECUTED"
+) -> List[Dict]:
     """
-    Функция принимает список словарей и опционально значение ключа state (по умолчанию
-    'EXECUTED'). Функция возвращает новый список словарей, содержащий только те словари, у
-     которых ключ state соответствует указанному значению
+    Функция принимает список словарей и опционально значение ключа state
+    (по умолчанию 'EXECUTED'). Функция возвращает новый список словарей,
+    содержащий только те словари, у которых ключ state соответствует
+    указанному значению
     """
 
     new_user_data = []
@@ -18,11 +21,15 @@ def filter_by_state(user_data_list: List[Dict], state: str = "EXECUTED") -> List
     return new_user_data
 
 
-def sort_by_date(date_info_list: List[Dict], reverse_order: bool = True) -> List[Dict]:
+def sort_by_date(
+    date_info_list: List[Dict], reverse_order: bool = True
+) -> List[Dict]:
     """
     Функция сортировки списка словарей по дате (по умолчанию - убывание)
     """
 
-    sorted_list_dict = sorted(date_info_list, key=lambda x: x["date"], reverse=reverse_order)
+    sorted_list_dict = sorted(
+        date_info_list, key=lambda x: x["date"], reverse=reverse_order
+    )
 
     return sorted_list_dict
