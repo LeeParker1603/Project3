7e8753f90a1dded404f8cc22e5bd3ade7873ea19
Evgenia Gorobets <leeparker1603@gmail.com>
2026-02-18 15:38:55 +0700

    add filter_by_currency with test for it

--- src/generators.py
+++ src/generators.py
@@ -1,5 +1,19 @@
-def filter_by_currency():
-    pass
+from typing import List, Any
+
+
+def filter_by_currency(transactions: List, currency_code: str) -> List:
+    """
+    Функция принимает на вход список словарей, представляющих транзакции,
+    и возвращает итератор, который поочередно выдает транзакции,
+    где валюта операции соответствует заданной (например, USD)
+    """
+    if not transactions:
+        raise ValueError("Необходимо ввести данные")
+
+    for transaction in transactions:
+        if transaction['operationAmount']['currency']['code'] == currency_code:
+            yield transaction
+
 
 
 def transaction_descriptions():
---
+++ tests/test_generators.py
@@ -0,0 +1,54 @@
+from typing import List, Any
+
+import pytest
+
+from src.generators import (filter_by_currency,
+                            transaction_descriptions,
+                            card_number_generator)
+
+
+# Тест, что функция корректно фильтрует транзакции
+# по заданной валюте
+def test_filter_by_currency(transactions_list: List, transactions_result: List) -> None:
+    usd_transactions = list(filter_by_currency(transactions_list, "USD"))
+    assert len(usd_transactions) > 0  # Убедились, что есть транзакции в USD
+    assert all(transactions['operationAmount']['currency']['code'] == 'USD' for transactions in usd_transactions)
+    assert usd_transactions == transactions_result
+
+# Тест, что функция не завершается ошибкой при обработке
+# пустого списка
+# или списка без соответствующих валютных операций
+def test_filter_by_currency_empty(empty: Any) -> None:
+    currency_empty = filter_by_currency(empty, empty)
+    with pytest.raises(ValueError, match="Необходимо ввести данные"):
+        # Преобразуем в список, чтобы запустить генератор
+        list(currency_empty)
+
+
+# Тест, что функция возвращает корректные описания
+# для каждой транзакции
+def test_transaction_descriptions():
+    pass
+
+
+# Тестируем работу функции с различным количеством
+# входных транзакций, включая пустой список
+def test_transaction_descriptions_empty():
+    pass
+
+
+# Тест, который проверяет, что генератор выдает
+# правильные номера карт в заданном диапазоне
+def test_card_number_generator():
+    pass
+
+
+# Проверяем корректность форматирования номеров карт
+def test_card_number_generator_format():
+    pass
+
+
+# Проверяем, что генератор корректно обрабатывает крайние
+# значения диапазона и правильно завершает генерацию
+def test_card_number_generator_correct():
+    pass
