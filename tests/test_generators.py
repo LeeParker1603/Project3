efdea1d235cbd50d045a8dfbc4d2f42af2235bd5
Евгения Горобец <leeparker1603@gmail.com>
2026-02-27 23:34:57 +0700

    flake8 correct

--- tests/test_generators.py
+++ tests/test_generators.py
@@ -2,8 +2,11 @@ from typing import Any, Dict, List
 
 import pytest
 
-from src.generators import (card_number_generator, filter_by_currency,
-                            transaction_descriptions)
+from src.generators import (
+    card_number_generator,
+    filter_by_currency,
+    transaction_descriptions,
+)
 
 
 # Тест, что функция корректно фильтрует транзакции
